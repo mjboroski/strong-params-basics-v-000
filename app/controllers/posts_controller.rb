19fb38bf3e69cf8ff8120e7c6fa8cd7314eaf30e
Michael Boroski <mjboroski@gmail.com>
2018-06-19 21:21:40 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,9 +28,10 @@ class PostsController < ApplicationController
 	end
 
 	private
- 
-def post_params
-  params.require(:post).permit(:title, :description)
-end
+
+	def post_params(*args)
+	  params.require(:post).permit(*args)
+	end
+
 
 end
